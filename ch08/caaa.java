86fa36c949f3de28288a78d7776f2528e5c67f08
alligator123 <91953298+alligator123@users.noreply.github.com>
2021-10-05 19:42:03 +0800

    Add files via upload

---
+++ ch08/caaa.java
@@ -0,0 +1,7 @@
+package java08;
+
+public class caaa {
+int a;
+int b;
+int c;
+}
---
+++ ch08/class01.java
@@ -0,0 +1,16 @@
+package java08;
+
+public class class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		caaa obj = new caaa();
+		obj.a=5;
+		obj.b=3;
+		obj.c=obj.a*obj.b;
+		System.out.println("a="+obj.a);
+		System.out.println("b="+obj.b);
+		System.out.println("c="+obj.c);
+	}
+ 
+}
