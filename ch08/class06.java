2d9b86d4f1e9e0185f9353e3f73299553fe9cb80
alligator123 <91953298+alligator123@users.noreply.github.com>
2021-10-10 22:05:24 +0800

    Add files via upload

---
+++ ch08/class06.java
@@ -0,0 +1,11 @@
+
+public class class06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CCircle cir1 = new CCircle();
+		cir1.radius=3.0;
+		cir1.show_periphery();
+	}
+
+}
