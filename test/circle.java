ef70b618c37d6ad48877744c38b6ff75359bed01
alligator123 <91953298+alligator123@users.noreply.github.com>
2021-11-01 11:49:08 +0800

    Add files via upload

---
+++ test/circle.java
@@ -0,0 +1,25 @@
+
+public class circle {
+int radius;
+int Long;
+int height;
+int width; 
+double pi=3.14159;
+void Cal_area(int r) {
+	radius=r;
+	System.out.println("半徑="+radius);
+	System.out.println("球形體積="+radius*radius*radius*4/3*pi);
+}
+void Cal_area(int a,int b,int c) {
+	Long = a;
+	height = b;
+	width = c;
+	System.out.println("長="+Long+",寬="+width+",高="+height);
+	System.out.println("立方體表面積="+(Long*height*2+Long*width*2+height*width*2));
+}
+public static void main(String[] args) {
+	circle a = new circle();
+	a.Cal_area(10);
+	a.Cal_area(4,6,5);
+}
+}
