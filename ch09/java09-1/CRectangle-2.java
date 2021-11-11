3edc0405a3a94280bf8e1db5c91080e4bea75c48
alligator123 <91953298+alligator123@users.noreply.github.com>
2021-11-11 22:06:06 +0800

    Add files via upload

---
+++ ch09/java09-1/CRectangle-2.java
@@ -0,0 +1,23 @@
+package java09;
+
+public class CRectangle {
+private int width;
+private int height;
+public CRectangle(int w,int h) { //a
+	width=w;
+	height=h;
+}
+public CRectangle() { //c
+	this(10,8);
+}
+void show() {
+	System.out.println(width+","+height);
+}
+	public static void main(String[] args) {
+		CRectangle a = new CRectangle(8,10);
+		CRectangle b = new CRectangle();
+		a.show();
+		b.show();
+	}
+
+}
