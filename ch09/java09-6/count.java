c57ad5adc4a76a0273162f31e0e3c3c4e6593818
alligator123 <91953298+alligator123@users.noreply.github.com>
2021-11-13 14:10:57 +0800

    Add files via upload

---
+++ ch09/java09-6/count.java
@@ -0,0 +1,11 @@
+package java09;
+class count {
+private static void add2n(int n) {
+	 System.out.println((1+n)*n/2);
+}
+	public static void main(String[] args) {
+	 count.add2n(5);
+	 count.add2n(10);
+	}
+
+}
