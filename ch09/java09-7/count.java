c22fa1d9ab73b3e21d7e0e70382ae1a1c795bfa7
alligator123 <91953298+alligator123@users.noreply.github.com>
2021-11-13 14:01:09 +0800

    Add files via upload

---
+++ ch09/java09-7/count.java
@@ -0,0 +1,11 @@
+package java09;
+class count {
+private static void power(int a,int b) {
+	System.out.println((int)Math.pow(a,b));
+}
+	public static void main(String[] args) {
+	count.power(2,5);
+	count.power(3,2);
+	}
+
+}
