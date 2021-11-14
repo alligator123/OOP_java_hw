809ba2413629f836e324387ea0fa1ac3afd0da52
alligator123 <91953298+alligator123@users.noreply.github.com>
2021-11-14 23:05:20 +0800

    Update Caaa02.java

--- ch10/Caaa02.java
+++ ch10/Caaa02.java
@@ -1,1 +1,25 @@
 
+class Caaa {
+	private int num1;//宣告成private的成員不能直接在子類別存取，只能透過父類別的函數存取
+	private int num2;
+}
+
+class Cbbb extends Caaa {
+	private void set_num(int a, int b) {
+		num1 = a;
+		num2 = b;
+	}
+
+	private void show() {
+		System.out.println("num1=" + num1);
+		System.out.println("num2=" + num2);
+	}
+
+	public static void main(String[] args) {
+		Cbbb bb = new Cbbb();
+		bb.set_num(5, 10);
+		bb.show();
+	}
+
+}
+
