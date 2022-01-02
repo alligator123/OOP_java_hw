b4230cbf68047f278854e0c420cda2ea4948005f
alligator123 <91953298+alligator123@users.noreply.github.com>
2022-01-02 19:50:16 +0800

    Add files via upload

---
+++ ch12/hw12_1/CRectangle.java
@@ -0,0 +1,6 @@
+
+class CRectangle        // 定義CRectangle類別
+{
+   int width;           // 宣告資料成員width
+   int height;             // 宣告資料成員height
+}
---
+++ ch12/hw12_1/app8_1.java
@@ -0,0 +1,15 @@
+
+public class app8_1 {
+
+	   public static void main(String args[])
+	   {
+	      CRectangle rect1;
+	      rect1=new CRectangle(); // 建立新的物件
+
+	      rect1.width=20;      // 設定矩形rect1的寬
+	      rect1.height=15;     // 設定矩形rect1的高
+
+	      System.out.println("width="+rect1.width);    // 印出rect1.width
+	      System.out.println("height="+rect1.height);    // 印出rect1.height
+	   }
+}
