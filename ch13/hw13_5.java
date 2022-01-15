ad7e7ad781ff8f9b09a8574eff2483847e943e7a
alligator123 <91953298+alligator123@users.noreply.github.com>
2022-01-15 22:49:44 +0800

    Add files via upload

---
+++ ch13/hw13_5.java
@@ -0,0 +1,21 @@
+package java13;
+
+public class hw13_5 {
+
+	public static void main(String[] args) {
+int num =12;
+int den[]= {12,0,3,0,0,4};
+try {
+for (int i=0;i<10;i++) {
+	System.out.println("ans="+num/den[i]);
+}
+}
+catch(ArithmeticException e) {
+	
+}
+catch(ArrayIndexOutOfBoundsException e) {
+	
+}
+	}
+
+}
