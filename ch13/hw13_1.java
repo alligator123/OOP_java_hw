d89feec994fca942494b0f562503f886bfb75db0
alligator123 <91953298+alligator123@users.noreply.github.com>
2022-01-15 22:35:11 +0800

    Add files via upload

---
+++ ch13/hw13_1.java
@@ -0,0 +1,12 @@
+package java13;
+
+public class hw13_1 {
+
+	public static void main(String[] args) {
+		int num = 12,den=0;
+		int ans=num/den;
+		System.out.println("end of main()!!");
+
+	}
+
+}
